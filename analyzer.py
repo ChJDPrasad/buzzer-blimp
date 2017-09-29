17944e5c5909d816bcb899286bd3fcd08df5254a
ragnar <rahulgovind517@gmail.com>
2017-09-29 15:20:21 +0530

    New commit

--- analyzer.py
+++ analyzer.py
@@ -4,6 +4,7 @@ from sizing import find_a
 from constants import *
 import numpy as np
 import matplotlib.pyplot as plt
+from __future__ import print_function
 
 
 def print_info(tether, kite, envelope):
@@ -19,6 +20,7 @@ def print_info(tether, kite, envelope):
 
     print("Total weight", total_weight)
 
+
 def analyze(lk=5, z=1.6):
     kite = Kite(lk, 0.5 * 1.08 * lk, 0.)
     tether = Tether(100.)
@@ -27,19 +29,20 @@ def analyze(lk=5, z=1.6):
     envelope = Envelope(a, 1.6)
     sph_payload_weight = envelope.weight + w_excess
 
-    aoa = get_aerod_data(z,v, (sph_payload_weight)*g,envelope.buoyancy*g, kite.weight * g,
-                    envelope.ref_area, kite.horizontal_area,
-                    envelope.a, envelope.c, lk)["alpha"]
+    aoa = get_aerod_data(z, v, (sph_payload_weight) * g, envelope.buoyancy * g, kite.weight * g,
+                         envelope.ref_area, kite.horizontal_area,
+                         envelope.a, envelope.c, lk)["alpha"]
 
     Ty = calc_Ty(aoa, v, sph_payload_weight * g, kite.weight * g,
-                envelope.buoyancy * g, envelope.ref_area, kite.horizontal_area)
+                 envelope.buoyancy * g, envelope.ref_area, kite.horizontal_area)
     Tx = calc_Tx(aoa, v, envelope.ref_area, kite.horizontal_area)
 
     print('aoa: ' + str(aoa))
     print("Tx: ", Tx)
-    print("Ty: ", Ty)   
+    print("Ty: ", Ty)
     print("Blowby: ", tether.calc_blowby(Tx, Ty))
     print("Altitude: ", tether.calc_altitude(Tx, Ty))
+
 
 if __name__ == "__main__":
     analyze(6, 2)
--- stability.py
+++ stability.py
@@ -2,27 +2,34 @@ import numpy as np
 from numpy import sin, cos, tan
 from scipy.optimize import fsolve
 from constants import *
+from scipy.misc import derivative
 
 
 def Clk(alpha):
     return (0.9848 * (alpha) ** 2 + 0.7665 * (alpha) + 0.1002)
 
+
 def Cdk(alpha):
     return (1.8524 * (alpha) ** 2 - 0.1797 * (alpha) + 0.1536)
+
 
 def Cmk(alpha):
     return -(0.2939 * (alpha) ** 2 + 0.5189 * (alpha) - 0.1921)
 
+
 def Cl(alpha):
     return (-0.3269 * (alpha) ** 2 + 0.8036 * (alpha) + 0.0049)
+
 
 def Cd(alpha):
     return (0.3447 * (alpha) ** 2 + 0.0631 * (alpha) + 0.0989)
 
+
 def Cm(alpha):
     return -(0.2071 * (alpha) ** 2 - 0.5647 * (alpha) + 0.0012)
 
-def Mcp(alpha,z, q, W, B , Wk, S, Sk, a, b, cr):
+
+def Mcp(alpha, z, q, W, B, Wk, S, Sk, a, b, cr):
     Lk = q * Sk * Clk(alpha)
     Dk = q * Sk * Cdk(alpha)
     Mk = q * Sk * cr * Cmk(alpha)
@@ -30,13 +37,18 @@ def Mcp(alpha,z, q, W, B , Wk, S, Sk, a, b, cr):
     D = q * S * Cd(alpha)
     M = q * S * 2 * a * Cm(alpha)
 
-    return Wk * (z*sin(alpha) + cr / 3 * cos(alpha)) + (W - L - B) * (b+  z) * \
-           sin(alpha) - Lk * (z*sin(alpha)+ cr / 2 * cos(alpha)) + \
-           Mk + M + D * (b + z)* cos(alpha) + Dk * (z*cos(alpha) - cr / 2 * sin(alpha))
+    return Wk * (z * sin(alpha) + cr / 3 * cos(alpha)) + (W - L - B) * (b + z) * \
+                                                         sin(alpha) - Lk * (z * sin(alpha) + cr / 2 * cos(alpha)) + \
+           Mk + M + D * (b + z) * cos(alpha) + Dk * (z * cos(alpha) - cr / 2 * sin(alpha))
 
-def get_aerod_data(z, v, W, B,Wk, S, Sk, a, b, cr):
+
+def Mcp_alpha(alpha, z, q, W, B, Wk, S, Sk, a, b, cr):
+    return derivative(Mcp, alpha, args=(z, q, W, B, Wk, S, Sk, a, b, cr))
+
+
+def get_aerod_data(z, v, W, B, Wk, S, Sk, a, b, cr):
     q = 0.5 * rho_air * v ** 2
-    alpha = fsolve(Mcp, 0.1, args=(z, q, W,B, Wk, S, Sk, a, b, cr))[0]
+    alpha = fsolve(Mcp, 0.1, args=(z, q, W, B, Wk, S, Sk, a, b, cr))[0]
 
     Lk = q * Sk * Clk(alpha)
     Dk = q * Sk * Cdk(alpha)
@@ -45,8 +57,9 @@ def get_aerod_data(z, v, W, B,Wk, S, Sk, a, b, cr):
     D = q * S * Cd(alpha)
     M = q * S * 2 * a * Cm(alpha)
 
-    return {"alpha": alpha, "L": L, "Lk": Lk, "D": D,\
-                "Dk": Dk, "M": M, "Mk": Mk}
+    return {"alpha": alpha, "L": L, "Lk": Lk, "D": D, \
+            "Dk": Dk, "M": M, "Mk": Mk}
+
 
 def calc_Ty(alpha, v, W, Wk, B, S, Sk):
     q = 0.5 * rho_air * v ** 2
@@ -54,9 +67,9 @@ def calc_Ty(alpha, v, W, Wk, B, S, Sk):
     L = q * S * Cl(alpha)
     return L + Lk + B - W - Wk
 
+
 def calc_Tx(alpha, v, S, Sk):
     q = 0.5 * rho_air * v ** 2
     Dk = q * Sk * Cdk(alpha)
     D = q * S * Cd(alpha)
     return D + Dk
-
