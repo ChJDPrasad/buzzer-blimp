4ed0da9241f1a1062832429994cf21951ba7d51e
Harish Murali <harish2111996@gmail.com>
2017-09-29 13:38:33 +0530

    minor name changes

--- analyzer.py
+++ analyzer.py
@@ -1,25 +1,23 @@
 from common import Tether, Kite, Envelope
-from stability import get_alpha, calc_Tx, calc_Ty
+from stability import *
 from sizing import find_a
 from constants import *
 import numpy as np
 import matplotlib.pyplot as plt
-from stability import Cl, Cd, Cm, Clk, Cdk, Cmk, get_alpha
 
 
-# def print_info(tether, kite, envelope):
-#     total_weight = envelope.weight + kite.weight + tether.weight + w_excess
+def print_info(tether, kite, envelope):
+    total_weight = envelope.weight + kite.weight + tether.weight + w_excess
 
-#     print "Semi-major axis", envelope.a
-#     print "Semi-minor axis", envelope.c
+    print("Semi-major axis", envelope.a)
+    print("Semi-minor axis", envelope.c)
 
-#     print "Buoyancy", envelope.buoyancy
+    print("Buoyancy", envelope.buoyancy)
+    print("Tether weight", tether.weight)
+    print("Envelope weight", envelope.weight)
+    print("Kite weight", kite.weight)
 
-#     print "Tether weight", tether.weight
-#     print "Envelope weight", envelope.weight
-#     print "Kite weight", kite.weight
-
-#     print "Total weight", total_weight
+    print("Total weight", total_weight)
 
 def analyze(lk=5, z=1.6):
     kite = Kite(lk, 0.5 * 1.08 * lk, 0.)
@@ -29,9 +27,9 @@ def analyze(lk=5, z=1.6):
     envelope = Envelope(a, 1.6)
     sph_payload_weight = envelope.weight + w_excess
 
-    aoa = get_alpha(z,v, (sph_payload_weight)*g,envelope.buoyancy*g, kite.weight * g,
+    aoa = get_aerod_data(z,v, (sph_payload_weight)*g,envelope.buoyancy*g, kite.weight * g,
                     envelope.ref_area, kite.horizontal_area,
-                    envelope.a, envelope.c, lk)
+                    envelope.a, envelope.c, lk)["alpha"]
 
     Ty = calc_Ty(aoa, v, sph_payload_weight * g, kite.weight * g,
                 envelope.buoyancy * g, envelope.ref_area, kite.horizontal_area)
@@ -42,3 +40,6 @@ def analyze(lk=5, z=1.6):
     print("Ty: ", Ty)   
     print("Blowby: ", tether.calc_blowby(Tx, Ty))
     print("Altitude: ", tether.calc_altitude(Tx, Ty))
+
+if __name__ == "__main__":
+    analyze(6, 2)
--- stability.py
+++ stability.py
@@ -1,47 +1,26 @@
-# coding: utf-8
-
-# In[18]:
-
 import numpy as np
 from numpy import sin, cos, tan
 from scipy.optimize import fsolve
+from constants import *
 
-# In[22]:
-
-rho = 1.225  # kg/m^3
-
-
-# v = 9 # m/s
-# T = 303.15 # Kelvin
-
-
-# In[14]:
 
 def Clk(alpha):
     return (0.9848 * (alpha) ** 2 + 0.7665 * (alpha) + 0.1002)
 
-
 def Cdk(alpha):
     return (1.8524 * (alpha) ** 2 - 0.1797 * (alpha) + 0.1536)
-
 
 def Cmk(alpha):
     return -(0.2939 * (alpha) ** 2 + 0.5189 * (alpha) - 0.1921)
 
-
 def Cl(alpha):
     return (-0.3269 * (alpha) ** 2 + 0.8036 * (alpha) + 0.0049)
-
 
 def Cd(alpha):
     return (0.3447 * (alpha) ** 2 + 0.0631 * (alpha) + 0.0989)
 
-
 def Cm(alpha):
     return -(0.2071 * (alpha) ** 2 - 0.5647 * (alpha) + 0.0012)
-
-
-# In[20]:
 
 def Mcp(alpha,z, q, W, B , Wk, S, Sk, a, b, cr):
     Lk = q * Sk * Clk(alpha)
@@ -50,47 +29,34 @@ def Mcp(alpha,z, q, W, B , Wk, S, Sk, a, b, cr):
     L = q * S * Cl(alpha)
     D = q * S * Cd(alpha)
     M = q * S * 2 * a * Cm(alpha)
-    return Wk * (z*sin(alpha) + cr / 3 * cos(alpha)) + (W - L - B) * (b+  z) * sin(alpha) - Lk * (z*sin(alpha)+ cr / 2 * cos(alpha)) + \
+
+    return Wk * (z*sin(alpha) + cr / 3 * cos(alpha)) + (W - L - B) * (b+  z) * \
+           sin(alpha) - Lk * (z*sin(alpha)+ cr / 2 * cos(alpha)) + \
            Mk + M + D * (b + z)* cos(alpha) + Dk * (z*cos(alpha) - cr / 2 * sin(alpha))
 
-
-# In[23]:
-
-def get_alpha(z, v, W, B,Wk, S, Sk, a, b, cr):
-    q = 0.5 * rho * v ** 2
+def get_aerod_data(z, v, W, B,Wk, S, Sk, a, b, cr):
+    q = 0.5 * rho_air * v ** 2
     alpha = fsolve(Mcp, 0.1, args=(z, q, W,B, Wk, S, Sk, a, b, cr))[0]
 
-        
     Lk = q * Sk * Clk(alpha)
     Dk = q * Sk * Cdk(alpha)
     Mk = q * Sk * cr * Cmk(alpha)
     L = q * S * Cl(alpha)
     D = q * S * Cd(alpha)
     M = q * S * 2 * a * Cm(alpha)
-    print(L, Lk, D, Dk, M, Mk)
-    return alpha
 
-
-# In[25]:
+    return {"alpha": alpha, "L": L, "Lk": Lk, "D": D,\
+                "Dk": Dk, "M": M, "Mk": Mk}
 
 def calc_Ty(alpha, v, W, Wk, B, S, Sk):
-    q = 0.5 * rho * v ** 2
+    q = 0.5 * rho_air * v ** 2
     Lk = q * Sk * Clk(alpha)
     L = q * S * Cl(alpha)
-    # print "Ty calcs", L, Lk, W, Wk
     return L + Lk + B - W - Wk
 
-
 def calc_Tx(alpha, v, S, Sk):
-    q = 0.5 * rho * v ** 2
+    q = 0.5 * rho_air * v ** 2
     Dk = q * Sk * Cdk(alpha)
     D = q * S * Cd(alpha)
-    # print "Tx calcs", D, Dk
     return D + Dk
 
-
-if __name__ == "__main__":
-    print("askcklsa")
-    pass
-
-    # get_alpha(alpha,v,w,wk,s,sk)
